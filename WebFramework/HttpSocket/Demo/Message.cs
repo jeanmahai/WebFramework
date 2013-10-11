0941bad70e38a09f3714073bdf1f9c88692242a8
jeanmahai <mahai_1986@126.com>
2013-10-11 20:55:53 +0800

    sdfsd

---
+++ WebFramework/HttpSocket/Demo/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo
+{
+    public class Message
+    {
+        public bool IsPush { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public DateTime PullDate { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ WebFramework/HttpSocket/Demo/MessageQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo
+{
+    public static class MessageQueue
+    {
+        private static List<Message> Messages = new List<Message>();
+        private static object LockObject = new object();
+
+        public static List<Message> GetMessage(string to)
+        {
+            var query = (from a in Messages
+                         where a.To == to && a.IsPush == false
+                         select a).ToList();
+            query.ForEach(p =>
+            {
+                p.IsPush = true;
+                p.PullDate = DateTime.Now;
+            });
+            return query;
+        }
+        public static void AddMessage(Message message)
+        {
+            lock (LockObject)
+            {
+                Messages.Add(message);
+            }
+        }
+    }
+}
