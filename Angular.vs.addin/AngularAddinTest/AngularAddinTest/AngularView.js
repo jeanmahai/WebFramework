06b344bcb9cd871e283a1c0098aef3752a06fc79
jean-home <mahai_1986@126.com>
2014-03-15 23:18:15 +0800

    sadfsdf

--- Angular.vs.addin/AngularAddinTest/AngularAddinTest/AngularView.js
+++ Angular.vs.addin/AngularAddinTest/AngularAddinTest/AngularView.js
@@ -1,9 +1,8 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <title></title>
-</head>
-<body>
-fdfsdf
-</body>
-</html>
+
+
+
+(function(){
+	var _index = window.angular.callbacks.counter.toString(36) - 1;
+	var _html='<div>    <p>        sdfsdf</p></div>';
+	window.angular.callbacks['_' + _index](_html);
+})();
