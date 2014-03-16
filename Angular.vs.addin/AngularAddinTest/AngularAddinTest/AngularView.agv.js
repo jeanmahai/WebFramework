84fd6b1d92c9d74ab1eae95979568287c1ac6842
jean-home <mahai_1986@126.com>
2014-03-16 19:27:27 +0800

    123

--- Angular.vs.addin/AngularAddinTest/AngularAddinTest/AngularView.agv.js
+++ Angular.vs.addin/AngularAddinTest/AngularAddinTest/AngularView.agv.js
@@ -1,8 +1,5 @@
-
-
-
 (function(){
 	var _index = window.angular.callbacks.counter.toString(36) - 1;
-	var _html='<div>    <p>        sdfsdf</p></div>';
+	var _html='<div><p>        \'dfsdfsdfsdfsdfcxvcv sdf</p></div>';
 	window.angular.callbacks['_' + _index](_html);
 })();
---
+++ Angular.vs.addin/AngularAddinTest/AngularAddinTest/abc.agv.js
@@ -0,0 +1,8 @@
+
+
+
+(function(){
+	var _index = window.angular.callbacks.counter.toString(36) - 1;
+	var _html='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head>    <title></title></head><body>dsfd</body></html>';
+	window.angular.callbacks['_' + _index](_html);
+})();
